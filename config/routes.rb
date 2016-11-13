18be3eeb04d5178712993e50421dce9c21f26f89
Nick Warren <nwarren@harnessed.io>
2016-11-13 12:04:02 -0800

    Updated routes
    Added routes for Auth0 and user dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
-
+  
   # Set the homepage
   root 'homepage#index'
+
+  # Auth0 callbacks
+  get "/auth/auth0/callback" => "auth0#callback"
+  get "/auth/failure" => "auth0#failure"
+
+  # Page routes
+  get 'dashboard', to: 'dashboard#index'
+  get 'homepage/index'
 
 end
