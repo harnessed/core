2a254c3853e35d195c1c0ea69b5370f3e1392441
Nick Warren <nwarren@harnessed.io>
2016-11-13 15:00:35 -0800

    Added additional authentication logic and updates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,22 @@
 class DashboardController < ApplicationController
+	
   def index
+  	# Redirect user to homepage if they haven't logged in
+  	unless session[:userinfo].present?
+      redirect_to '/'
+    end
+
+    # Display the user's credentials
+  	@user = session[:userinfo]
   end
+
+  def logout
+  	# Destroy the session variable and user data
+    session[:userinfo] = nil
+
+    # Display a logged out message
+    flash[:notice] = "You've been logged out, see you next time!"
+    redirect_to '/'
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,11 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    #config.cache_store = :null_store
+
+    # Address the Auth0 session overflow issue
+    config.cachestore = :memorystore
+
   end
 
   # Don't care if the mailer can't send.
@@ -51,4 +55,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
