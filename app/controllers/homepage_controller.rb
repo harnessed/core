c372e75cc9444000cca3c6369767222c7b07abbb
Nick Warren <nwarren@harnessed.io>
2016-11-13 16:38:56 -0800

    Added an authentication check to the homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,11 @@
 class HomepageController < ApplicationController
   def index
+  	# Redirect user to homepage if they haven't logged in
+  	if session[:userinfo].present?
+      redirect_to '/dashboard'
+    end
+
+    # Display the user's credentials
+  	@user = session[:userinfo]
   end
 end
